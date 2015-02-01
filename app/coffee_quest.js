8bf0de47113e9ffefc5e37ec916ea33bcef042ec
thethirddan <thethirddan@gmail.com>
2015-02-01 13:33:31 -0800

    DUNGEON IS FUNCTIONAL

--- app/coffee_quest.js
+++ app/coffee_quest.js
@@ -20,7 +20,7 @@ var explore = function(floorSquare, dungeon){
 		//TODO: call square encounter
 
 		//change square data (visibility)
-		setSquareData(floorSquare, 'clear', '');
+		setSquareData(floorSquare, 'clearSquare', '');
 
 		//render dungeon
 		//TODO: should we do this, or just change the 4 squares around it?
@@ -40,7 +40,7 @@ var createDungeon = function(dimX, dimY) {
     	for (y = 0; y < dimY; y++) { 
     		dungeon[x][y] = {};
 
-    		dungeon[x][y].vis = 'hidden';
+    		dungeon[x][y].vis = 'darkSquare';
     		dungeon[x][y].enc = '';
 
 		}
@@ -71,17 +71,17 @@ var discoverEncounter = function(floorSquare, dungeon) {
 
 var createDungeonStart = function(dungeon, dimX, dimY) {
 	//pick a random square and make it blank
-	var randX = Math.floor((Math.random() * dimX) + 1); 
-	var randY = Math.floor((Math.random() * dimY) + 1);
+	var randX = Math.floor((Math.random() * dimX)); 
+	var randY = Math.floor((Math.random() * dimY));
 	var randomFloorSquare = dungeon[randX][randY];
 
-	setSquareData(randomFloorSquare, 'clear','');
+	dungeon[randX][randY] = setSquareData(randomFloorSquare, 'clearSquare','');
 
 	return dungeon;
 };
 
 var setDungeonVisibility = function(dungeon){
-	var vis; //clear, hidden or clickable
+	var vis; //clearSquare, darkSquare or clickableSquare
 	var squareVis;
 	//loop through all squares
 	for (var x = 0; x < dungeon.length; x++) {
@@ -89,8 +89,8 @@ var setDungeonVisibility = function(dungeon){
 
 			squareVis = dungeon[x][y].vis;
 			
-			if (squareVis != 'clear' && squareNextToVisible(dungeon, x, y)) {
-				vis = 'clickable';
+			if (squareVis != 'clearSquare' && squareNextToVisible(dungeon, x, y)) {
+				vis = 'clickableSquare';
 			} else {
 				vis = squareVis;
 			}
@@ -114,19 +114,20 @@ var setSquareData = function(floorSquare, visibility, encounter){
 
 
 var renderDungeonHTML = function(dungeon){
-	console.log(dungeon);
+	//console.log(dungeon);
 
 	var dungeonHTML = "<div class='row'>";
 
 	for (var x = 0; x < dungeon.length; x++) {
 		for (var y = 0; y < dungeon[x].length; y++) {
-			dungeonHTML += "<div class='" + dungeon[x][y].vis + "'>" + dungeon[x][y].vis + "</div>";
+			dungeonHTML += "<div class='col-xs-2 "+ dungeon[x][y].vis +"'>" + dungeon[x][y].vis + "</div>";
 		}
 		dungeonHTML += "</div><div class='row'>";
 	}
 
 	dungeonHTML += "</div>";
 
+	//console.log(dungeonHTML);
 	return dungeonHTML;
 }
 
@@ -137,9 +138,10 @@ var renderSquareHTML = function(floorSquare){
 
 
 var squareNextToVisible = function(dungeon, floorSquareX, floorSquareY){
-	console.log("x: "+ floorSquareX +",y: "+ floorSquareY);
-	var xLen = dungeon.length;
-	var yLen = dungeon[0].length;
+	//the minus one is for the array position.
+	var xLen = dungeon.length - 1;
+	var yLen = dungeon[0].length - 1;
+
 
 	var sq0vis = dungeon[floorSquareX][floorSquareY].vis;
 	var sq1vis;
@@ -181,7 +183,7 @@ var squareNextToVisible = function(dungeon, floorSquareX, floorSquareY){
 
 var diffSquares = function(sq1vis, sq2vis) {
 	//if one square is visible and one square isn't
-	if ((sq1vis == "clear" && (sq2vis == "hidden" || sq2vis == "clickable")) || (sq2vis == "clear" && (sq1vis == "hidden" || sq1vis == "clickable"))) {
+	if ((sq1vis == "clearSquare" && (sq2vis == "darkSquare" || sq2vis == "clickableSquare")) || (sq2vis == "clearSquare" && (sq1vis == "darkSquare" || sq1vis == "clickableSquare"))) {
 		return true;
 	} else {
 		return false;
