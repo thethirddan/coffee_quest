c992f56bbf877316ec75a07bc9bbc379101ab1e9
Nathan Lyle Black <lyleblack@gmail.com>
2015-02-02 11:05:35 -0800

    start outline for coffee_quest.js tests

---
+++ app/coffee_quest_test.js
@@ -0,0 +1,31 @@
+
+// describe:
+//    #createArray
+//      it: creates array of proper length if 1 arg
+//      it: creates nested array of proper size if 1+ args
+//    #explore
+//    #createDungeon
+//      it: creates dungeon of specified size
+//      it: includes start point
+//      it: includes clickable squares near start point
+//      it: is rendered in valid HTML
+//    #discoverEncounter
+//    #createDungeonStart
+//      it: chooses start point that's within dungeon bounds
+//      it: updates dungeon to include that point
+//    #setDungeonVisibility
+//      it: sets squares near visible to clickable
+//      it: doesn't change any other squares
+//    #setSquareData
+//      it: updates visibility attr
+//      it: updates cncounter attr
+//    #renderDungeonHTML
+//      it: outputs valid html
+//    #renderSquareHTML
+//    #squareNextToVisible
+//      it: returns true if given square is next to visible
+//      it: returns false if given square is not next to visible
+//    #diffSquares
+//      it: returns true if one square is visible and other isn't
+//      it: returns false if both are visible
+//      it: returns false if both aren't visible
